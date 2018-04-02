90e961a414e44acba857459f34f43ded12f7f24d
Alexandre Braganca <atb@isep.ipp.pt>
2018-04-02 10:14:59 +0100

    Added server address parameter to ClientApp.

--- src/main/java/net/jnjmx/todd/ClientApp.java
+++ src/main/java/net/jnjmx/todd/ClientApp.java
@@ -13,18 +13,22 @@ public class ClientApp {
 	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 		System.out.println("Todd Client App...");
-		System.out.println("Connecting to todd server");
 		
 		try {
-
-			System.out.println("Connecting...");
+			String server = "192.168.56.11";
 			
-			Client c=new Client("192.168.56.11");
+			if (args.length >= 1) {
+				server = args[0];
+			}			
+
+			System.out.println("Connecting to TODD server at "+server+" ...");
+			
+			Client c=new Client(server);
 
 			System.out.println("The current time of day on todd server is: "+c.timeOfDay());
 			
-			System.out.println("Waiting 60 secs to receive notifications");
-			Thread.sleep(60000);
+//			System.out.println("Waiting 60 secs to receive notifications");
+//			Thread.sleep(60000);
 			
 			c.close();
 			
