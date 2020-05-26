20766896b9bb49f5dbf5dacfa26c02c42f204039
silv3rio <silverio@live.com>
2020-05-26 12:11:02 +0100

    Update nexus_starter.sh

--- nexus_starter.sh
+++ nexus_starter.sh
@@ -3,7 +3,7 @@
 /etc/init.d/freeradius start >/dev/null 2>/dev/null
 /etc/init.d/tacacs_plus start >/dev/null 2>/dev/null
 service ssh start
-/root/nexus-3.23.0-03/bin/nexus start
+/nexus-3.23.0-03/bin/nexus start
 
 clear
 
