7c500b33ad51f18c21be8390dade3f2c7a2e8bc0
silv3rio <silverio@live.com>
2020-06-01 15:13:01 +0100

    Update starter.sh

--- starter.sh
+++ starter.sh
@@ -4,7 +4,9 @@
 /etc/init.d/tacacs_plus start >/dev/null 2>/dev/null
 service ssh start
 #java -cp /root/cogsiP5-latest.jar net.jnjmx.todd.Server &
-
+java -Dcom.sun.management.jmxremote.port=10500 -Dcom.sun.management.jmxremote.rmi.port=10500 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Djava.rmi.server.hostname=192.168.20.20 -Dcom.sun.management.jmxremote.host=192.168.20.20 -cp /root/cogsiP5-latest.jar net.jnjmx.todd.Server & echo $! >/root/todd.pid
+sleep 5
+java -cp /root/cogsiP5-latest.jar net.jnjmx.todd.ClientApp4 & echo $! >/root/client.pid
 clear
 
 # Launching shell
