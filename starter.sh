565b698098beac5c679824fb698b7a935e9d3fda
silv3rio <silverio@live.com>
2020-06-01 14:15:49 +0100

    Update starter.sh

--- starter.sh
+++ starter.sh
@@ -3,7 +3,7 @@
 /etc/init.d/freeradius start >/dev/null 2>/dev/null
 /etc/init.d/tacacs_plus start >/dev/null 2>/dev/null
 service ssh start
-java -cp /root/cogsiP5-latest.jar net.jnjmx.todd.Server &
+#java -cp /root/cogsiP5-latest.jar net.jnjmx.todd.Server &
 
 clear
 
